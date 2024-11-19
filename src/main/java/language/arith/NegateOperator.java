38d4d98ab1e6e7a9f83a57b879b1461ee34f0aac
Owen Hallock <owenhallock29@gmail.com>
2024-11-19 10:00:40 -0500

    Update NegateOperator.java

--- src/main/java/language/arith/NegateOperator.java
+++ src/main/java/language/arith/NegateOperator.java
@@ -2,20 +2,13 @@ package language.arith;
 
 import language.Operand;
 
-/**
- * The {@link NegateOperator} is an operator that performs negation
- * on a single integer.
- * @author jcollard, jddevaug
- */
 public class NegateOperator extends UnaryOperator<Integer> {
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public Operand<Integer> performOperation() {
-        // TODO: Follow the example from PlusOperator to override
-        //   this method (from the version in UnaryOperator)
-        //   for negation.
-        return null;
+        Operand<Integer> operand = getOperand();
+        if (operand == null) {
+            throw new IllegalStateException("Operand has not been set.");
+        }
+        return new Operand<>(-operand.getValue());
     }
 }
