ebc47fec5376be82ae5ad3aeffc76d19d87c401a
Owen Hallock <owenhallock29@gmail.com>
2024-11-19 10:01:05 -0500

    Update DivOperator.java

--- src/main/java/language/arith/DivOperator.java
+++ src/main/java/language/arith/DivOperator.java
@@ -9,24 +9,39 @@ import language.Operand;
  * @author jcollard, jddevaug
  */
 public class DivOperator extends BinaryOperator<Integer> {
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Operand<Integer> performOperation() {
-        // TODO: Follow the example from PlusOperator to override
-        //   this method (from the version in BinaryOperator)
-        //   for division.
-        return null;
-    }
 
     /**
      * {@inheritDoc}
      */
     @Override
+    public Operand<Integer> performOperation() {
+        Operand<Integer> op0 = this.getOp0();
+        Operand<Integer> op1 = this.getOp1();
+        if (op0 == null || op1 == null) {
+            throw new IllegalStateException("Could not perform operation prior to operands being set.");
+        }
+
+        // Check for division by zero
+        if (op1.getValue() == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+
+        Integer result = op0.getValue() / op1.getValue();
+        return new Operand<>(result);
+    }
+
+    /**
+     * {@inheritDoc}
+     * This method is overridden to check for division by zero when operands are set.
+     */
+    @Override
     public void setOperand(int i, Operand<Integer> operand) {
-        // TODO: For division we need to additionally override
-        //  the setOperand method to check for division by zero.
-        //  See DivOperatorTest (and the README) for usage.
+        // Ensure that operand i is set to the appropriate operand
+        super.setOperand(i, operand);
+
+        // Additional check to prevent division by zero during operand assignment
+        if (i == 1 && operand.getValue() == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
     }
 }
